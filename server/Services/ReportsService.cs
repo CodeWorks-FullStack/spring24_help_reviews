fd5ef15acb65b980ed708fb8439bd4d33aa2f86b
Jeremy Fowler <jembyfowler@gmail.com>
2024-05-22 16:20:49 -0600

    added checks for creating reports

--- server/Services/ReportsService.cs
+++ server/Services/ReportsService.cs
@@ -1,23 +1,26 @@
 
-
-
-
-
-
-
 namespace help_reviews.Services;
 
 public class ReportsService
 {
   private readonly ReportsRepository _repository;
+  private readonly RestaurantsService _restaurantsService;
 
-  public ReportsService(ReportsRepository repository)
+  public ReportsService(ReportsRepository repository, RestaurantsService restaurantsService)
   {
     _repository = repository;
+    _restaurantsService = restaurantsService;
   }
 
   internal Report CreateReport(Report reportData)
   {
+    Restaurant restaurant = _restaurantsService.GetRestaurantById(reportData.RestaurantId, reportData.CreatorId);
+
+    if (reportData.CreatorId == restaurant.CreatorId)
+    {
+      throw new Exception($"Hey there {restaurant.Creator.Name}, you can't leave a report for {restaurant.Name}, because you own it! That is a conflict of interest");
+    }
+
     Report report = _repository.Create(reportData);
     return report;
   }
